41e75a7b178367c1aceec37998decf08cb25f050
Ethan Qiu HOME <balancein0and1@gmail.com>
2019-06-20 00:57:16 -0400

    Jun 20 00:57

--- Compose.py
+++ Compose.py
@@ -1,36 +1,73 @@
 from magenta.protobuf import music_pb2
-#from google.colab import files
+# from google.colab import files
 
 import magenta.music as mm
 import magenta
 import tensorflow
+
 drums = music_pb2.NoteSequence()
-piano=music_pb2.NoteSequence();
-piano.notes.add(is_piano=True)
-drums.notes.add(pitch=36, start_time=0, end_time=0.125, is_drum=True, instrument=10, velocity=80)
-drums.notes.add(pitch=38, start_time=0, end_time=0.125, is_drum=True, instrument=10, velocity=80)
-drums.notes.add(pitch=42, start_time=0, end_time=0.125, is_drum=True, instrument=10, velocity=80)
-drums.notes.add(pitch=46, start_time=0, end_time=0.125, is_drum=True, instrument=10, velocity=80)
-drums.notes.add(pitch=42, start_time=0.25, end_time=0.375, is_drum=True, instrument=10, velocity=80)
-drums.notes.add(pitch=42, start_time=0.375, end_time=0.5, is_drum=True, instrument=10, velocity=80)
-drums.notes.add(pitch=42, start_time=0.5, end_time=0.625, is_drum=True, instrument=10, velocity=80)
-drums.notes.add(pitch=50, start_time=0.5, end_time=0.625, is_drum=True, instrument=10, velocity=80)
-drums.notes.add(pitch=36, start_time=0.75, end_time=0.875, is_drum=True, instrument=10, velocity=80)
-drums.notes.add(pitch=38, start_time=0.75, end_time=0.875, is_drum=True, instrument=10, velocity=80)
-drums.notes.add(pitch=42, start_time=0.75, end_time=0.875, is_drum=True, instrument=10, velocity=80)
-drums.notes.add(pitch=45, start_time=0.75, end_time=0.875, is_drum=True, instrument=10, velocity=80)
-drums.notes.add(pitch=36, start_time=1, end_time=1.125, is_drum=True, instrument=10, velocity=80)
-drums.notes.add(pitch=42, start_time=1, end_time=1.125, is_drum=True, instrument=10, velocity=80)
-drums.notes.add(pitch=46, start_time=1, end_time=1.125, is_drum=True, instrument=10, velocity=80)
-drums.notes.add(pitch=42, start_time=1.25, end_time=1.375, is_drum=True, instrument=10, velocity=80)
-drums.notes.add(pitch=48, start_time=1.25, end_time=1.375, is_drum=True, instrument=10, velocity=80)
-drums.notes.add(pitch=50, start_time=1.25, end_time=1.375, is_drum=True, instrument=10, velocity=80)
-drums.total_time = 1.375
+piano = music_pb2.NoteSequence();
 
-drums.tempos.add(qpm=60)
+twinkle_twinkle = music_pb2.NoteSequence()
 
-# This creates a file called `drums_sample_output.mid`, containing the drums solo we've been using.
-mm.sequence_proto_to_midi_file(drums, 'drums_sample_output.mid')
+# Add the notes to the sequence.
+twinkle_twinkle.notes.add(pitch=60, start_time=0.0, end_time=0.5, velocity=80)
+twinkle_twinkle.notes.add(pitch=60, start_time=0.5, end_time=1.0, velocity=80)
+twinkle_twinkle.notes.add(pitch=67, start_time=1.0, end_time=1.5, velocity=80)
+twinkle_twinkle.notes.add(pitch=67, start_time=1.5, end_time=2.0, velocity=80)
+twinkle_twinkle.notes.add(pitch=69, start_time=2.0, end_time=2.5, velocity=80)
+twinkle_twinkle.notes.add(pitch=69, start_time=2.5, end_time=3.0, velocity=80)
+twinkle_twinkle.notes.add(pitch=67, start_time=3.0, end_time=4.0, velocity=80)
+twinkle_twinkle.notes.add(pitch=65, start_time=4.0, end_time=4.5, velocity=80)
+twinkle_twinkle.notes.add(pitch=65, start_time=4.5, end_time=5.0, velocity=80)
+twinkle_twinkle.notes.add(pitch=64, start_time=5.0, end_time=5.5, velocity=80)
+twinkle_twinkle.notes.add(pitch=64, start_time=5.5, end_time=6.0, velocity=80)
+twinkle_twinkle.notes.add(pitch=62, start_time=6.0, end_time=6.5, velocity=80)
+twinkle_twinkle.notes.add(pitch=62, start_time=6.5, end_time=7.0, velocity=80)
+twinkle_twinkle.notes.add(pitch=60, start_time=7.0, end_time=8.0, velocity=80)
+twinkle_twinkle.total_time = 8
+
+twinkle_twinkle.tempos.add(qpm=60);
 
 # This is a colab utility method to download that file. In your Python script, you
 # would just write it to disk.
+print("Downloading model bundle. This will take less than a minute...")
+mm.notebook_utils.download_bundle('basic_rnn.mag', '/content/')
+bundle = mm.sequence_generator_bundle.read_bundle_file('/content/basic_rnn.mag')
+
+# Import dependencies.
+from magenta.models.melody_rnn import melody_rnn_sequence_generator
+from magenta.protobuf import generator_pb2
+from magenta.protobuf import music_pb2
+
+# Initialize the model.
+
+generator_map = melody_rnn_sequence_generator.get_generator_map()
+melody_rnn = generator_map['basic_rnn'](checkpoint=None, bundle=bundle)
+melody_rnn.initialize()
+print("INITIALIZATION COMPLETE")
+
+# Model options. Change these to get different generated sequences!
+
+input_sequence = twinkle_twinkle # change this to teapot if you want
+num_steps = 128 # change this for shorter or longer sequences
+temperature = 1.0 # the higher the temperature the more random the sequence.
+
+# Set the start time to begin on the next step after the last note ends.
+last_end_time = (max(n.end_time for n in input_sequence.notes)
+                  if input_sequence.notes else 0)
+qpm = input_sequence.tempos[0].qpm
+seconds_per_step = 60.0 / qpm / melody_rnn.steps_per_quarter
+total_seconds = num_steps * seconds_per_step
+
+generator_options = generator_pb2.GeneratorOptions()
+generator_options.args['temperature'].float_value = temperature
+generate_section = generator_options.generate_sections.add(
+  start_time=last_end_time + seconds_per_step,
+  end_time=total_seconds)
+
+# Ask the model to continue the sequence.
+sequence = melody_rnn.generate(input_sequence, generator_options)
+mm.sequence_proto_to_midi_file(sequence, 'filler_sample_output.mid')
+mm.plot_sequence(sequence)
+mm.play_sequence(sequence, synth=mm.fluidsynth)
